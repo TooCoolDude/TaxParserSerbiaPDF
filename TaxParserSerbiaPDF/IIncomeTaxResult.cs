86f00eb42c7a892b59165f68066a5ac383d4aae1
TooCoolDude <122090417+TooCoolDude@users.noreply.github.com>
2024-03-14 18:54:28 +0500

    re-init

---
+++ TaxParserSerbiaPDF/IIncomeTaxResult.cs
@@ -0,0 +1,10 @@
+﻿public interface IIncomeTaxResult
+{
+    int FirstYear {get;}
+    DateTime FirstMonthStartDate {get;}
+    DateTime FirstMonthEndDate {get;}
+    decimal FirstMonthPayment {get;}
+    decimal RegularPayment {get;}
+    byte[] FirstYearQRpng {get;}
+    byte[] NextYearQRpng {get;}
+}
